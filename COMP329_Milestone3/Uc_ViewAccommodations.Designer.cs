d96ccc1ebb8ed65e29b88406611d497bc97af154
Wing <wyy3@students.waikato.ac.nz>
2018-06-05 19:55:04 +1200

    View Accommodations user control added

---
+++ COMP329_Milestone3/Uc_ViewAccommodations.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace COMP329_Milestone3
+{
+    partial class Uc_ViewAccommodations
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lb_AName = new System.Windows.Forms.Label();
+            this.lb_Address = new System.Windows.Forms.Label();
+            this.lb_PhoneNO = new System.Windows.Forms.Label();
+            this.lb_Email = new System.Windows.Forms.Label();
+            this.btn_ViewRoom = new System.Windows.Forms.Button();
+            this.lb_Description = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lb_AName
+            // 
+            this.lb_AName.AutoSize = true;
+            this.lb_AName.Font = new System.Drawing.Font("Calibri", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lb_AName.Location = new System.Drawing.Point(24, 10);
+            this.lb_AName.Name = "lb_AName";
+            this.lb_AName.Size = new System.Drawing.Size(266, 33);
+            this.lb_AName.TabIndex = 1;
+            this.lb_AName.Text = "Accommodation Name";
+            this.lb_AName.UseMnemonic = false;
+            // 
+            // lb_Address
+            // 
+            this.lb_Address.AutoSize = true;
+            this.lb_Address.Font = new System.Drawing.Font("Calibri", 12.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lb_Address.Location = new System.Drawing.Point(27, 87);
+            this.lb_Address.Name = "lb_Address";
+            this.lb_Address.Size = new System.Drawing.Size(66, 21);
+            this.lb_Address.TabIndex = 2;
+            this.lb_Address.Text = "Address";
+            // 
+            // lb_PhoneNO
+            // 
+            this.lb_PhoneNO.AutoSize = true;
+            this.lb_PhoneNO.Font = new System.Drawing.Font("Calibri", 12.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lb_PhoneNO.Location = new System.Drawing.Point(27, 108);
+            this.lb_PhoneNO.Name = "lb_PhoneNO";
+            this.lb_PhoneNO.Size = new System.Drawing.Size(78, 21);
+            this.lb_PhoneNO.TabIndex = 3;
+            this.lb_PhoneNO.Text = "Phone No";
+            // 
+            // lb_Email
+            // 
+            this.lb_Email.AutoSize = true;
+            this.lb_Email.Font = new System.Drawing.Font("Calibri", 12.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lb_Email.Location = new System.Drawing.Point(175, 108);
+            this.lb_Email.Name = "lb_Email";
+            this.lb_Email.Size = new System.Drawing.Size(48, 21);
+            this.lb_Email.TabIndex = 4;
+            this.lb_Email.Text = "Email";
+            // 
+            // btn_ViewRoom
+            // 
+            this.btn_ViewRoom.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_ViewRoom.Location = new System.Drawing.Point(398, 52);
+            this.btn_ViewRoom.Name = "btn_ViewRoom";
+            this.btn_ViewRoom.Size = new System.Drawing.Size(98, 32);
+            this.btn_ViewRoom.TabIndex = 5;
+            this.btn_ViewRoom.Text = "View Rooms";
+            this.btn_ViewRoom.UseVisualStyleBackColor = true;
+            this.btn_ViewRoom.Click += new System.EventHandler(this.btn_ViewRoom_Click);
+            // 
+            // lb_Description
+            // 
+            this.lb_Description.AutoSize = true;
+            this.lb_Description.Font = new System.Drawing.Font("Calibri", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lb_Description.Location = new System.Drawing.Point(26, 52);
+            this.lb_Description.Name = "lb_Description";
+            this.lb_Description.Size = new System.Drawing.Size(99, 23);
+            this.lb_Description.TabIndex = 6;
+            this.lb_Description.Text = "Description";
+            // 
+            // Uc_ViewAccommodations
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightSteelBlue;
+            this.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Controls.Add(this.lb_Description);
+            this.Controls.Add(this.btn_ViewRoom);
+            this.Controls.Add(this.lb_Email);
+            this.Controls.Add(this.lb_PhoneNO);
+            this.Controls.Add(this.lb_Address);
+            this.Controls.Add(this.lb_AName);
+            this.Name = "Uc_ViewAccommodations";
+            this.Size = new System.Drawing.Size(515, 145);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lb_AName;
+        private System.Windows.Forms.Label lb_Address;
+        private System.Windows.Forms.Label lb_PhoneNO;
+        private System.Windows.Forms.Label lb_Email;
+        private System.Windows.Forms.Button btn_ViewRoom;
+        private System.Windows.Forms.Label lb_Description;
+    }
+}
---
+++ COMP329_Milestone3/Uc_ViewAccommodations.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OracleClient;
+
+namespace COMP329_Milestone3
+{
+    public partial class Uc_ViewAccommodations : UserControl
+    {
+        public Uc_ViewAccommodations()
+        {
+            InitializeComponent();
+        }
+
+        public Accommodation data
+        {
+            get; set;
+        }
+
+        public void DataBind()
+        {
+            lb_AName.Text = data.AName;
+            lb_Description.Text = data.Description;
+            lb_Address.Text = data.Address;
+            lb_PhoneNO.Text = data.PhoneNo;
+            lb_Email.Text = data.Email;
+        }
+
+        private void btn_ViewRoom_Click(object sender, EventArgs e)
+        {
+            ViewRooms rooms = new ViewRooms();
+            rooms.AID = data.AID;
+            rooms.AName = data.AName;
+            rooms.ShowDialog();
+        }
+    }
+}
